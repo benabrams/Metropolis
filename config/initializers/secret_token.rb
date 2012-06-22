dfd41b8a5b305c22e3ef8c3b3b8b6e25e0bc48c0
Ben Abrams <ben@benabrams.com>
2012-06-22 16:48:32 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Metropolis::Application.config.secret_token = 'b81a0fc89efdbc9bde13385685f1d7626030113d731d3714120c6d8bb24a756428dfb1f573d2a1a6f059242436f89a938601c7152ebade5e0c62989632c4a715'
